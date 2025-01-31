26607053627f89d7f32c13ee50e5f8b144abc135
Samuel <samroberval@gmail.com>
2025-01-31 00:02:31 +0000

    feature: change event image size

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -1,21 +1,22 @@
 import { TbCalendar as CalendarIcon } from "react-icons/tb";
 import { Link } from "react-router-dom";
+import { Utils } from "../utils";
 
 export function EventItem(props) {
+    const {getExcerpt} = Utils.String;
     return (
         <div className="shadow-sm bg-white">
             <div className="row g-0">
                 <div className="col-12 col-lg-4">
-                    <img className="img-fluid h-100" src={props.event.img_url} 
-                    alt={props.event.name} loading="lazy"
-                    style={{
+                    <img src={props.event.img_url} width={255} height={255} 
+                    alt={props.event.name} loading="lazy" style={{
                         objectFit: 'cover', 
                         maxHeight: '400px', 
                         objectPosition: 'center'
                     }}/>
                 </div>
                 <div className="col-12 col-lg-8 p-3">
-                    <div className="p-3 p-lg-0 pr-lg-4">
+                    <div className="p-3 pr-md-4">
                         <div className="text-uppercase mb-0">
                             {props.event.name}
                         </div>
@@ -24,7 +25,7 @@ export function EventItem(props) {
                             <CalendarIcon size={18} /> 
                             {new Date(props.event.date).toLocaleDateString()}
                         </div>
-                        <p>{props.event.description}</p>
+                        <p>{getExcerpt(props.event.description)}</p>
                         <Link className="btn btn-primary btn-sm" to={`/galleries/${props.event.id}`}>
                             en savoir plus
                         </Link>
--- src/utils/String.js
+++ src/utils/String.js
@@ -4,6 +4,14 @@ const __ = (key) => {
     return (key in langFr) ? langFr[key] : key;
 }
 
+const getExcerpt = (text, length = 180) => {
+    if (!text) return '';
+    if (text.length <= length) return text;
+
+    return text.substring(0, length).concat('...');
+}
+
 export const String = {
-    __
+    __,
+    getExcerpt,
 }
